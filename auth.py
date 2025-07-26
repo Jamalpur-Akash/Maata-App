c7398f1b6497a6762a1abbf1e7a08daa04301819
Jamalpur-Akash <akashjamalpur9@gmail.com>
2025-07-26 18:49:49 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,29 +6,47 @@ USERS = {"test": "1234"}
 def login_signup():
     tab1, tab2 = st.tabs(["🔐 **లాగిన్**", "🆕 **సైన్అప్**"])
 
-    with tab1:
-        # Username
-        st.markdown("<label style='font-weight: bold; margin-top: 2px;'>వినియోగదారుని పేరు</label>", unsafe_allow_html=True)
+    with tab1:# Username
+       st.markdown("<label style='font-weight: bold;'>వినియోగదారుని పేరు</label>", unsafe_allow_html=True)
+       uname = st.text_input("", key="username")
+
+       st.write("")  # Adds space between inputs
+
+# Password
+       st.markdown("<label style='font-weight: bold;'>పాస్వర్డ్</label>", unsafe_allow_html=True)
+       pwd = st.text_input("", type="password", key="password")
+
+       if st.button("లాగిన్"):
+         if USERS.get(uname) == pwd:
+           st.session_state.logged_in = True
+           st.session_state.username = uname
+         else:
+            st.error("తప్పు లాగిన్ వివరాలు")
+
+        
+ 
+
+        """st.markdown("### 🔐 *లాగిన్*")
+        #uname = st.text_input("**వినియోగదారుని  పేరు**")
+        #pwd = st.text_input("**పాస్వర్డ్**", type="password")
+        st.markdown("""<label style='font-weight: bold; display: block; margin-bottom: 6px;'>వినియోగదారుని పేరు</label>""",unsafe_allow_html=True)
         uname = st.text_input("", key="username")
 
-        # Small spacer
-        st.markdown("<div style='margin-top: 2px;'></div>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
 
-        # Password
-        st.markdown("<label style='font-weight: bold; margin-top: 2px;'>పాస్వర్డ్</label>", unsafe_allow_html=True)
-        pwd = st.text_input("", type="password", key="password")
-
+        st.markdown("""<label style='font-weight: bold; display: block; margin-bottom: 6px;'>పాస్వర్డ్</label>""",unsafe_allow_html=True)
+        pwd = st.text_input("", type="password", key="password")
         if st.button("లాగిన్"):
             if USERS.get(uname) == pwd:
                 st.session_state.logged_in = True
                 st.session_state.username = uname
             else:
-                st.error("తప్పు లాగిన్ వివరాలు")
+                st.error("తప్పు లాగిన్ వివరాలు")"""
 
     with tab2:
         st.markdown("### 🆕 *సైన్అప్*")
-        new_user = st.text_input("**కొత్త వినియోగదారుని పేరు**")
-        new_pwd = st.text_input("**కొత్త పాస్వర్డ్**", type="password")
+        new_user = st.text_input("**కొత్త  వినియోగదారుని  పేరు**")
+        new_pwd = st.text_input("**కొత్త  పాస్వర్డ్**", type="password")
         if st.button("ఖాతా సృష్టించండి"):
             if new_user in USERS:
                 st.warning("ఈ వినియోగదారుని పేరు ముందు నుంచే ఉంది!")
