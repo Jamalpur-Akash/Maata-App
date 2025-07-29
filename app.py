10740b4ad06f1aca8888da2afe7be0f5b10e153f
Nanda kishor Reddy Nelapatla <nelapatlanandakishore@gmail.com>
2025-07-29 10:15:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, storage
 
-# Init Firebase
-if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': '<your-bucket-name>.appspot.com'  # e.g., maata-app.appspot.com
-    })
-
-db = firestore.client()
-bucket = storage.bucket()
 # Our brand new app
 import streamlit as st
 import os
